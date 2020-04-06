472408e96ef086c49693e1448cbf01e6d56051b8
Emil Karimov <emvakar@gmail.com>
2020-04-06 05:22:43 +0300

    3.8.3

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -10,6 +10,6 @@ let package = Package(
     ],
     dependencies: [],
     targets: [
-        .target(name: "FSCalendar", dependencies: [], path: "FSCalendar"),
+        .target(name: "FSCalendar", dependencies: [], path: "FSCalendar", publicHeadersPath: "FSCalendar"),
     ]
 )
